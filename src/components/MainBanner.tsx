865449775021a593a3284444f049ffeb6fd16b68
wslee <gntcjswo@gmail.com>
2024-08-13 15:34:58 +0900

    메인 코드 정리

--- src/components/MainBanner.tsx
+++ src/components/MainBanner.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import styles from './MainBanner.module.scss';
 import { Link } from 'react-router-dom';
 import Slick from './slider/Slick';
