beb47ab917433e99d2ebf20d7094c09eed02f8cf
이우성 <webfavorite@naver.com>
2024-08-25 14:09:17 +0900

    not found 페이지 완료

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import styles from './NotFound.module.scss';
 
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className={styles.notfound}>
+      <h2>페이지를 찾을 수 없습니다.</h2>
+      <figure>
+        <img src='/img/img_404_not_found.gif' alt='404 Not Found' />
+      </figure>
+    </div>
+  );
 }
